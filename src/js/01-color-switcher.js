98ff471602b7c730898297a7f420e60654c0e8ec
Sanderrica <99506840+Sanderrica@users.noreply.github.com>
2023-04-13 22:12:34 +0300

    add task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,31 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let timerId = null;
+const CHANGE_COLOR_DELAY = 1000;
+btnStop.disabled = true;
+
+btnStart.addEventListener('click', onBntStart);
+btnStop.addEventListener('click', onBtnStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function onBntStart() {
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+
+    timerId = setInterval(() => {
+        body.style.backgroundColor = getRandomHexColor()
+    }, CHANGE_COLOR_DELAY);
+}
+
+function onBtnStop() {
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+
+    clearInterval(timerId);
+}
 
---
+++ src/js/02-timer.js
@@ -0,0 +1,110 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from 'notiflix';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+Notiflix.Notify.init({
+  width: '280px',
+  position: 'right-top',
+  distance: '10px',
+  opacity: 1,
+});
+
+const TIMER_DELAY = 1000;
+let selectedTime = null;
+
+const refs = {
+  body: document.querySelector('body'),
+  inputDate: document.querySelector('#datetime-picker'),
+  startBtnDate: document.querySelector('[data-start]'),
+  days: document.querySelector('[data-days]'),
+  hours: document.querySelector('[data-hours]'),
+  minutes: document.querySelector('[data-minutes]'),
+  seconds: document.querySelector('[data-seconds]'),
+}
+
+class Timer {
+  constructor() {
+    this.intervalId = null;
+    this.isActive = false;
+  }
+
+    start() {
+    if (this.isActive) {
+      return;
+    }
+    const startTime = Date.now();
+    this.isActive = true;
+
+    this.intervalId = setInterval(() => {
+      const currentTime = Date.now();
+      const deltaTime = selectedTime - currentTime;
+      const timeComponents = convertMs(deltaTime);
+      this.updateTimer(timeComponents);
+    }, TIMER_DELAY)
+  }
+  stop() {
+    clearInterval(this.intervalId);
+    this.isActive = false;
+  }
+
+  updateTimer({ days, hours, minutes, seconds }) {
+    refs.days.innerHTML = addLeadingZero(days);
+    refs.hours.innerHTML = addLeadingZero(hours);
+    refs.minutes.innerHTML = addLeadingZero(minutes);
+    refs.seconds.innerHTML = addLeadingZero(seconds);
+ }
+}
+
+refs.startBtnDate.addEventListener('click', () => {
+  timer.start();
+});
+
+const timer = new Timer();
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+const options = {
+  enableTime: true,  
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+      if (selectedDates[0] < Date.now()) {
+        Notify.failure('🥺 Ooops... Please, choose a date in the future');
+        refs.startBtnDate.disabled = true;
+      } else {
+        Notify.success('🥰 Congratulation! Click on start!');
+        refs.startBtnDate.disabled = false;
+        selectedTime = selectedDates[0];
+      }
+    },
+};
+
+flatpickr(refs.inputDate, options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
